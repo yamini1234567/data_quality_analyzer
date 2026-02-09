ab007ff6dcf486c6ea1d0e530b0f0845b46c5fe5
mrbunkar <mrbunkar200@gmail.com>
2026-02-09 22:38:32 +0530

    Refactored the code to add query models

---
+++ ai_core/shared/models.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel
+from beanie import Document
+from typing import Optional
+from enum import StrEnum, IntEnum
+from datetime import datetime
+
+
+class QueryType(StrEnum):
+    charge = "charge"
+    diagnosis = "diagnosis"
+    adjustment = "adjustment"
+    payment = "payment"
+
+
+class Priority(IntEnum):
+    high = 1
+    medium = 2
+    low = 3
+    neutral = 4
+
+
+class Query(BaseModel):
+    name: str
+    qtype: QueryType
+    desc: Optional[str] = None
+
+    pipeline: list[dict]
+    is_active: bool = True
+    priority: Priority
+
+
+class MQuery(Document, Query):
+    class Settings:
+        name = "data.query"
+
+
+class Result(BaseModel):
+    query_name: str
+    status: str
+    executed_at: datetime
+
+    result: dict | list[dict]
+    error: Optional[str] = None
+
+
+class MResult(Document, Result):
+    class Settings:
+        name = "data.validation"
